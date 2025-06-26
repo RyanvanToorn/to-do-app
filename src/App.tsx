08e02a93636be381b2bd8dc77c86ffc2879508cd
DarkLineRSA <ryanvantoorn@gmail.com>
2025-06-27 00:06:00 +0200

    Further documentation, UI Mockup updates, as well as adding some new components to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Button from "@components/Button";
 import Title from "@components/Title";
 import AppBar from "@components/AppBar";
+import Card from "@components/Card";
+import TextField from "@components/TextField";
 import { useDeviceClass } from "./hooks/useDeviceClass";
 
 function App() {
-  // Applies a conditional class to the <body> element.
+  // Applies a conditional class to the <body> element for device specific styling.
   useDeviceClass();
 
   return (
@@ -13,6 +15,9 @@ function App() {
         <Title text="To-Do" />
       </AppBar>
 
+      <Card />
+
+      <TextField isVisible={false} />
       <Button variant="outlined">Add New +</Button>
     </div>
   );
