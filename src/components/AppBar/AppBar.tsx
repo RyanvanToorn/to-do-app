71f23d7ca2d5812238db8299b0d4cbb6eada7ac8
DarkLineRSA <ryanvantoorn@gmail.com>
2025-06-26 18:24:35 +0200

    Initial commit - creation of the app bar component and implementation within the app

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AppBar as MUIAppBar } from "@mui/material";
+import type { AppBarProps } from "./AppBar.types";
+
+/**
+ * AppBar Component
+ * Wrapper to allow future library swap with minimal impact.
+ */
+
+const Card: React.FC<AppBarProps> = ({ children, ...props }) => {
+  return (
+    <MUIAppBar color="primary" {...props}>
+      {children}
+    </MUIAppBar>
+  );
+};
+
+export default Card;
