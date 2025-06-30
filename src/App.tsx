acff2c8b98856c07ee2230921afc219915161a8a
DarkLineRSA <ryanvantoorn@gmail.com>
2025-06-30 19:21:03 +0200

    Further documentation, added newly wrapped versions of the card to App.tsx for initial testing, and documented some additional learnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Button from "@components/Button";
 import Title from "@components/Title";
 import AppBar from "@components/AppBar";
-import Card from "@components/Card";
+import { Card } from "@components/Card";
+import { TodoCard } from "@components/Card";
+import { CounterCard } from "@components/Card";
 import TextField from "@components/TextField";
 import { useDeviceClass } from "./hooks/useDeviceClass";
 
 function App() {
-  // Applies a conditional class to the <body> element for device specific styling.
   useDeviceClass();
 
   return (
@@ -16,6 +17,8 @@ function App() {
       </AppBar>
 
       <Card />
+      <TodoCard />
+      <CounterCard title="Cancelled" count={5}></CounterCard>
 
       <TextField isVisible={false} />
       <Button variant="outlined">Add New +</Button>
