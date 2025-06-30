3e1482a5388f848bd67f6050e2d384f8aa6d16c9
DarkLineRSA <ryanvantoorn@gmail.com>
2025-06-30 19:28:49 +0200

    Changed the title and app bar to static positioning

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import type { TitleProps } from "./Title.types";
 import TitleStyles from "./Title.module.css";
 
-const Title: React.FC<TitleProps> = ({ text = "To-Do", isVisible = true, extendedClass = "", inlineStyles = {} }) => {
+/**
+ * Title Component
+ * Intended to be used wherever a large/central title is required.
+ */
+
+export const Title: React.FC<TitleProps> = ({ titleText = "To-Do", isVisible = true, extendedClass = "", inlineStyles = {} }) => {
   if (!isVisible) return null;
 
   return (
     <div className={`${TitleStyles.TitleWrapper} title-wrapper ${extendedClass}`} style={inlineStyles}>
       <div className={`${TitleStyles.Title} title`}>
-        <label className={`${TitleStyles.Label} title-label`}>{text}</label>
+        <label className={`${TitleStyles.Label} title-label`}>{titleText}</label>
       </div>
     </div>
   );
 };
-
-export default Title;
